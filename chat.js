f9ad72cbc6703219facaa28f2b925491ee5ecb7f
Paula Pinochet <paula.pinochet.p@gmail.com>
2015-04-27 16:20:30 -0400

    edición de index, css y js

--- chat.js
+++ chat.js
@@ -1,37 +1,36 @@
-
-function Mensaje(mensaje){
-				this.mensaje = desc;
+function Mensaje(chat){
+				this.chat = chat;
 			}
 
-Mensaje.mensajitos = [];
+Mensaje.mensajes = [];
 
 Mensaje.enviar = function(e){
 	e.preventDefault(); // previene comportamiento por defecto del formulario
 	var mensaje = document.getElementById("title").value;
 	var mensajito = new Mensaje(mensaje);
 
-	Mensaje.mensajito.push(mensajito);
+	Mensaje.mensajes.push(mensajito);
 	Mensaje.mostrar();
 	Mensaje.guardar(); 
 
 }
 
 Mensaje.mostrar = function(){ // metodo de clase
-	var comentarios = "";
-	Mensaje.mensajito.map(function(elemento, indice){
-		comentarios = comentarios + "<article>"+"<h2>" + // arreglar plantilla
-		elemento.mensaje + "</h2><p>" + "</p></article>";
+	var conversacion = "";
+	Mensaje.mensajes.map(function(elemento, indice){
+		conversacion = conversacion + "<p>" + elemento.chat + "</p>"
+	 ;
 	});
 
 
-document.getElementById("comentarios").innerHTML = comentarios;
+document.getElementById("conversacion").innerHTML = conversacion;
 
 }
 
 Mensaje.guardar =function () {
 	var mensajes = Mensaje.mensajes;
-	var twitsEncode = JSON.stringify(mensajes);
-	localStorage.setItem("mensajes", mensajesEncode); // esto toma nuestros twits, los convierte en un string con json y los guarda en el localstorage.
+	var mensajesEncode = JSON.stringify(mensajes);
+	localStorage.setItem("mensajes", mensajesEncode); 
 }
 
 Mensaje.obtenerMensajes = function () {
@@ -44,5 +43,5 @@ Mensaje.obtenerMensajes = function () {
 }
 
 
-Mensaje.obtenerMensajes();
+// Mensaje.obtenerMensajes();
 Mensaje.mostrar();
